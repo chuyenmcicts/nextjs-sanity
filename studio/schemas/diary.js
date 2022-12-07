f88b314830b75874ca63f767451e77c539b7952d
Viet Mai <viet.mai@uac.edu.au>
2022-12-07 14:07:50 +0700

    add schema to test

---
+++ studio/schemas/diary.js
@@ -0,0 +1,39 @@
+export default {
+  name: "diary",
+  title: "Diary",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Diary Title",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+    },
+    {
+      name: "image",
+      title: "Diary Image",
+      type: "image",
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "array",
+      of: [
+        {
+          title: "Block",
+          type: "block",
+          styles: [{ title: "Normal", value: "normal" }],
+          lists: [],
+        },
+      ],
+    },
+  ],
+};
