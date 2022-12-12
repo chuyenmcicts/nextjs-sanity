7ca9e71676d32769670835fc2b742ed459a2c240
Viet Mai <viet.mai@uac.edu.au>
2022-12-12 09:34:38 +0700

    add draw-in schema

---
+++ studio/schemas/draw-in.js
@@ -0,0 +1,17 @@
+export default {
+  name: "drawin",
+  title: "Draw In",
+  type: "document",
+  fields: [
+    {
+      name: "image",
+      title: "Draw In Image",
+      type: "image",
+    },
+    {
+      name: "file",
+      title: "Draw In File",
+      type: "file",
+    },
+  ],
+};
